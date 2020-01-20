0e18cad54f139fde26b28e3b7386045a1d9f88bb
Alice Mwihaki <alicemwihaki99@gmail.com>
2020-01-20 16:50:42 +0300

    final commit

--- manage.py
+++ manage.py
@@ -1,39 +1,27 @@
-# Import db from app factory
-from app import create_app,db
-from flask_script import Manager,Server
-from app.models import *
-# Set up migrations
-from flask_migrate import Migrate,MigrateCommand
+from flaskblog import create_app,db
 
-# Creating app instance
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager, Server
+from flaskblog.models import *
 
-app = create_app('production')
-
-
-
-# Create manager instance 
+# app = create_app('production')
+app = create_app('development')
 manager = Manager(app)
-
-# Create migrate instance
-migrate = Migrate(app,db)
-
-manager.add_command('server',Server)
-manager.add_command('db',MigrateCommand)
+migrate = Migrate(app, db)
+manager.add_command('server', Server)
+manager.add_command('db', MigrateCommand)
 
 @manager.command
 def test():
-    '''
-    Run the unit tests
-    '''
+    """Run the unit tests."""
     import unittest
-    tests = unittest.TestLoader().discover('tests')
+    tests = unittest.TestLoader().discover('test')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 
 @manager.shell
 def make_shell_context():
-    return dict( app=app, db=db, User=User, Pitch = Pitch, Comment=Comment)
-
+    return dict(app = app, db = db, User= User)
 
 if __name__ == '__main__':
     manager.run()
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-export MAIL_USERNAME='alicemwihaki99@gmail.com'
-export MAIL_PASSWORD='malofa@1998'
+export MAIL_USERNAME='ccayreen24@gmail.com'
+export MAIL_PASSWORD='huhahuha'
 export SECRET_KEY='secret'
-# SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:alice1234@localhost/alice'
+# SQLALCHEMY_DATABASE_URI='DATABASE_URL'
 python3.6 manage.py server
