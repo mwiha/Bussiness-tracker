88c17510b050d3e996f4e3040b5e53af0ad15c6a
Alice Mwihaki <alicemwihaki99@gmail.com>
2020-01-21 09:26:54 +0300

    deploy

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-export MAIL_USERNAME='ccayreen24@gmail.com'
-export MAIL_PASSWORD='huhahuha'
+export MAIL_USERNAME='alicemwihaki99@gmail.com'
+export MAIL_PASSWORD='alice'
 export SECRET_KEY='secret'
 # SQLALCHEMY_DATABASE_URI='DATABASE_URL'
 python3.6 manage.py server
